21d2f522a363e3abae1f522726e8ae1dfe44bc32
Brian Wortman <brian.wortman@outlook.com>
2014-06-12 11:23:30 -0400

    through the ch5 database txn control section

--- src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
+++ src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -1,14 +1,15 @@
 ﻿using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
-using log4net.Config; //
+using log4net.Config;
 using NHibernate;
 using NHibernate.Context;
-using Ninject; //
+using Ninject;
 using Ninject.Activation;
 using Ninject.Web.Common;
-using WebApi2Book.Common; //
-using WebApi2Book.Common.Logging; //
+using WebApi2Book.Common;
+using WebApi2Book.Common.Logging;
 using WebApi2Book.Data.SqlServer.Mapping;
+using WebApi2Book.Web.Common;
 
 namespace WebApi2Book.Web.Api
 {
@@ -48,6 +49,7 @@ namespace WebApi2Book.Web.Api
 
             container.Bind<ISessionFactory>().ToConstant(sessionFactory);
             container.Bind<ISession>().ToMethod(CreateSession).InRequestScope();
+            container.Bind<IActionTransactionHelper>().To<ActionTransactionHelper>().InRequestScope();
         }
 
         private ISession CreateSession(IContext context)
--- src/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
+++ src/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Net.Http;
 using System.Web.Http;
 using WebApi2Book.Web.Api.Models;
+using WebApi2Book.Web.Common;
 using WebApi2Book.Web.Common.Routing;
 
 namespace WebApi2Book.Web.Api.Controllers.V1
 {
     [ApiVersion1RoutePrefix("tasks")]
+    [UnitOfWorkActionFilter]
     public class TasksController : ApiController
     {
         [Route("", Name = "AddTaskRoute")]
