f2b86fea1cda26e9662f054b211a5276d1316cd1
Brian Wortman <brian.wortman@outlook.com>
2014-06-12 11:02:34 -0400

    through the ch5 nhibernate section

---
+++ src/WebApi2Book.Common/IDateTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApi2Book.Common
+{
+    public interface IDateTime
+    {
+        DateTime UtcNow { get; }
+    }
+}
+
+
--- src/WebApi2Book.Data/Entities/Status.cs
+++ src/WebApi2Book.Data/Entities/Status.cs
@@ -2,7 +2,7 @@
 
 namespace WebApi2Book.Data.Entities
 {
-    public class Status
+    public class Status : IVersionedEntity
     {
         public virtual long StatusId { get; set; }
         public virtual string Name { get; set; }
