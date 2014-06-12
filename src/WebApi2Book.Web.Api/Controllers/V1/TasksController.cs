f661f0ce05fec3d9d26b03d7087ca77bf8f603b1
Brian Wortman <brian.wortman@outlook.com>
2014-06-12 15:39:14 -0400

    through ch6 securing the POST

--- src/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
+++ src/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
@@ -1,9 +1,10 @@
-﻿using System.Net.Http;
-using System.Web.Http;
-using WebApi2Book.Web.Api.Models;
-using WebApi2Book.Web.Common;
-using WebApi2Book.Web.Common.Routing;
-using WebApi2Book.Web.Api.MaintenanceProcessing;
+﻿using System.Net.Http;//
+using System.Web.Http;//
+using WebApi2Book.Common;//
+using WebApi2Book.Web.Api.Models;//
+using WebApi2Book.Web.Common;//
+using WebApi2Book.Web.Common.Routing;//
+using WebApi2Book.Web.Api.MaintenanceProcessing;//
 
 namespace WebApi2Book.Web.Api.Controllers.V1
 {
@@ -20,6 +21,7 @@ namespace WebApi2Book.Web.Api.Controllers.V1
 
         [Route("", Name = "AddTaskRoute")]
         [HttpPost]
+        [Authorize(Roles = Constants.RoleNames.Manager)]
         public IHttpActionResult AddTask(HttpRequestMessage requestMessage, NewTask newTask)
         {
             var task = _addTaskMaintenanceProcessor.AddTask(newTask);
